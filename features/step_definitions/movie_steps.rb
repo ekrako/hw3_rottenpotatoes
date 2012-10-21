09ac528df24b3eb78218a095d75c2b7f4fa41a7d
Eran Krakovsky <ekrako@gmail.com>
2012-10-21 23:51:02 +0200

    finished with filter

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -22,7 +22,37 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(", ").each do |e| 
+    if uncheck
+      "when I uncheck #{e}" 
+    else
+      "when I check #{e}" 
+    end
+  end
+end  
+Then /I should( not)? see the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(", ").each do |e| 
+    if uncheck
+      "Then I should not see <td>#{e}" 
+    else
+      "Then I should see <td>#{e}" 
+    end
+  end
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+end
+
+Then /^I should see all of the movies$/ do
+  "Then I should see the following ratings: #{Movie.all_ratings.join(', ')}"
+end
+
+Given /^I (un)?check all the checkboxes$/ do |uncheck|
+  Movie.all_ratings.each do |e| 
+    if uncheck
+      "when I uncheck #{e}" 
+    else
+      "when I check #{e}" 
+    end
+  end
 end
